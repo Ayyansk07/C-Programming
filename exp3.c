648d464440e2e76966af7ab210bd13561dd988ba
Ayyansk07 <shaikhayyan07@eng.rizvi.edu.in>
2025-10-17 00:54:50 +0530

    Create exp3.c

---
+++ exp3.c
@@ -0,0 +1,57 @@
+/*
+Name : Shaikh Ayyan
+UIN - 251P001
+CLASS - COMP 1
+DIV - C
+*/
+#include <stdio.h>
+
+int main() {
+    int choice;
+    double num1, num2, result;
+    menu:
+    printf("\n--- Simple Calculator Menu ---\n");
+    printf("1. Addition\n");
+    printf("2. Subtraction\n");
+    printf("3. Multiplication\n");
+    printf("4. Division\n");
+    printf("5. Exit\n");
+    printf("Enter your choice: ");
+    scanf("%d", &choice);
+
+    if (choice >= 1 && choice <= 4) {
+        printf("Enter first number: ");
+        scanf("%lf", &num1);
+        printf("Enter second number: ");
+        scanf("%lf", &num2);
+    }
+
+    switch (choice) {
+        case 1:
+            result = num1 + num2;
+            printf("Result: %.2lf + %.2lf = %.2lf\n", num1, num2, result);
+            break;
+        case 2:
+            result = num1 - num2;
+            printf("Result: %.2lf - %.2lf = %.2lf\n", num1, num2, result);
+            break;
+        case 3:
+            result = num1 * num2;
+            printf("Result: %.2lf * %.2lf = %.2lf\n", num1, num2, result);
+            break;
+        case 4:
+            if (num2 != 0) {
+                result = num1 / num2;
+                printf("Result: %.2lf / %.2lf = %.2lf\n", num1, num2, result);
+            } else {
+                printf("Error: Division by zero is not allowed.\n");
+            }
+            break;
+        case 5:
+            printf("Exiting calculator. Goodbye!\n");
+            return 0; 
+        default:
+            printf("Invalid choice. Please try again.\n");
+    }
+    goto menu; 
+}
